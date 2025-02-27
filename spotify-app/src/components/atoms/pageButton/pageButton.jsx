dc277a4a62506ae9640f6d732c9067f6aee46d82
Sularada <111848753+Sularada@users.noreply.github.com>
2025-02-27 19:32:25 +0300

    Update pageButton.jsx

--- spotify-app/src/components/atoms/pageButton/pageButton.jsx
+++ spotify-app/src/components/atoms/pageButton/pageButton.jsx
@@ -2,7 +2,7 @@ import { StyledPageButton } from "../../styledComponents/PageButton";
 import PropTypes from "prop-types";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../../themes/darkTheme/DarkPageButtonTheme";
-export default function PageButton({ label, startIcon }) {
+export default function PageButton({ label, startIcon, variant, color }) {
   return (
     <ThemeProvider theme={theme}>
       <StyledPageButton startIcon={startIcon} variant="contained" color="dark">
